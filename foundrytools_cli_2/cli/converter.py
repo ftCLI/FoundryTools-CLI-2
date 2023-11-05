25ddc3add5c06ff65a6ce5dadc5812a65869eba5
ftCLI <ftCLI@proton.me>
2023-11-05 12:48:27 +0100

    Refactor converter.py to use logger, correct_otf_contours, and subroutinize functions

--- foundrytools_cli_2/cli/converter.py
+++ foundrytools_cli_2/cli/converter.py
@@ -1,7 +1,8 @@
-import logging
+from io import BytesIO
 from pathlib import Path
 from typing import Optional
 
+from cffsubr import subroutinize
 import click
 
 from foundrytools_cli_2.lib.click_options import (
@@ -13,22 +14,18 @@ from foundrytools_cli_2.lib.click_options import (
     target_upm_option,
     tolerance_option,
 )
+from foundrytools_cli_2.lib.font import Font
 from foundrytools_cli_2.lib.font_finder import (
     FontFinder,
     FontFinderError,
     FontFinderFilters,
     FontLoadOptions,
 )
+from foundrytools_cli_2.lib.logger import logger
 from foundrytools_cli_2.lib.timer import Timer
+from foundrytools_cli_2.lib.skia_tools import correct_otf_contours
 from foundrytools_cli_2.snippets.otf_to_ttf import otf_to_ttf
 from foundrytools_cli_2.snippets.ttf_to_otf import ttf_to_otf, get_charstrings
-
-logger = logging.getLogger(__name__)
-handler = logging.StreamHandler()
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setLevel(logging.NOTSET)
-handler.setFormatter(formatter)
-logger.addHandler(handler)
 
 
 cli = click.Group()
@@ -41,7 +38,7 @@ cli = click.Group()
 @output_dir_option()
 @overwrite_flag()
 @recalc_timestamp_flag()
-@Timer()
+@Timer(logger=logger.info)
 def ps2tt(
     input_path: Path,
     recursive: bool = False,
@@ -68,12 +65,12 @@ def ps2tt(
     for font in fonts:
         with font:
             try:
-                print(font.reader.file.name)
+                logger.info(f"Converting {font.reader.file.name}")
                 tt = otf_to_ttf(font=font, max_err=tolerance, reverse_direction=True)
                 out_file = tt.get_output_file(output_dir=output_dir, overwrite=overwrite)
                 font.save(out_file)
             except Exception as e:  # pylint: disable=broad-except
-                print(e)
+                logger.error(e)
 
 
 @cli.command("ttf2otf")
@@ -84,7 +81,7 @@ def ps2tt(
 @output_dir_option()
 @overwrite_flag()
 @recalc_timestamp_flag()
-@Timer()
+@Timer(logger=logger.info)
 def tt2ps(
     input_path: Path,
     recursive: bool = False,
@@ -110,23 +107,36 @@ def tt2ps(
         raise click.Abort(e)
 
     for font in fonts:
-        # with font:
-        try:
-            logger.info("Converting %s", font.reader.file.name)
+        with font:
+            try:
+                logger.info(f"Converting {font.reader.file.name}")
 
-            logger.info("Decomponentizing source font...")
-            font.tt_decomponentize()
-            if target_upm:
-                logger.info(f"Scaling UPM to {target_upm}")
-                font.tt_scale_upm(units_per_em=target_upm)
+                logger.info("Decomponentizing source font...")
+                font.tt_decomponentize()
+                if target_upm:
+                    logger.info(f"Scaling UPM to {target_upm}")
+                    font.tt_scale_upm(units_per_em=target_upm)
 
-            logger.info("Getting charstrings...")
-            charstrings = get_charstrings(font=font, tolerance=tolerance)
+                logger.info("Getting charstrings...")
+                charstrings = get_charstrings(font=font, tolerance=tolerance)
 
-            logger.info("Converting to OTF...")
-            ps = ttf_to_otf(font=font, charstrings=charstrings)
+                logger.info("Converting to OTF...")
+                ps = ttf_to_otf(font=font, charstrings=charstrings)
 
-            out_file = ps.get_output_file(output_dir=output_dir, overwrite=overwrite)
-            font.save(out_file)
-        except Exception as e:  # pylint: disable=broad-except
-            print(e)
+                buffer = BytesIO()
+                ps.save(buffer)
+                buffer.seek(0)
+                ps = Font(buffer, recalc_timestamp=False)
+                logger.error(ps.reader.file)
+
+                logger.info("Correcting contours...")
+                correct_otf_contours(ps)
+
+                logger.info("Subroutinizing...")
+                subroutinize(ps)
+
+                out_file = font.get_output_file(output_dir=output_dir, overwrite=overwrite)
+                ps.save(out_file)
+                logger.success(f"Saved {out_file}")
+            except Exception as e:  # pylint: disable=broad-except
+                print(e)
