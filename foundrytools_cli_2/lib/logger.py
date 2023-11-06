925c99b8deb146e2df21dff088faeb5ea24b10ab
ftCLI <ftCLI@proton.me>
2023-11-06 20:21:56 +0100

    Added LogFilter to change the log level of the existing handler

--- foundrytools_cli_2/lib/logger.py
+++ foundrytools_cli_2/lib/logger.py
@@ -4,14 +4,27 @@ from functools import partialmethod
 
 from loguru import logger
 
+
+class LoggerFilter:
+
+    def __init__(self, level):
+        self.level = level
+
+    def __call__(self, record):
+        level_no = logger.level(self.level).no
+        return record["level"].no >= level_no
+
+
+logger_filter = LoggerFilter("INFO")
+
 # Remove the default logger
-logger.remove(0)
+logger.remove()
 
 # Add a sink to the logger to print to stdout
 logger.add(
-    sys.stdout,
-    level="INFO",
-    backtrace=False,
+    sys.stderr,
+    filter=logger_filter,
+    backtrace=True,
     colorize=True,
     format="[ <level>{level: <8}</level> ] " "<level>{message}</level>",
 )
@@ -21,4 +34,4 @@ logger.level("SKIP", no=27, color="<light-black><bold>", icon="⏭️")
 logger.__class__.skip = partialmethod(logger.__class__.log, "SKIP")
 
 
-__all__ = ["logger"]
+__all__ = ["logger", "logger_filter"]
