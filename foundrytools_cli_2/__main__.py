81a1796c251f0ee8809b592fdc690168872da738
ftCLI <ftCLI@proton.me>
2023-11-06 20:23:16 +0100

    Enhanced CLI with version option

--- foundrytools_cli_2/__main__.py
+++ foundrytools_cli_2/__main__.py
@@ -2,4 +2,12 @@ import click
 
 from foundrytools_cli_2.cli.converter import cli as converter_cli
 
-cli = click.Group("ft-cli", commands={"converter": converter_cli})
+
+@click.group(
+    commands={
+        "converter": converter_cli,
+    }
+)
+@click.version_option()
+def cli():
+    pass
