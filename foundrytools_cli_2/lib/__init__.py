db636d7879b11549cc5331dd70feec19efacea41
ftCLI <ftCLI@proton.me>
2023-12-04 20:56:46 +0100

    Remove unused TimerError from __init__.py

--- foundrytools_cli_2/lib/__init__.py
+++ foundrytools_cli_2/lib/__init__.py
@@ -1,14 +1,14 @@
 from .font import Font
 from .font_finder import FontFinder, FontFinderError, FontFinderFilter
 from .logger import logger, LoggerFilter
-from .timer import Timer, TimerError
+from .timer import Timer
 
 __all__ = [
     "Font",
     "FontFinder",
     "FontFinderError",
+    "FontFinderFilter",
     "logger",
     "LoggerFilter",
     "Timer",
-    "TimerError",
 ]
