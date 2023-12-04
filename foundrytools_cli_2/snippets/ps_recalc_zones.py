494811cd61a8b6840ec4c3424c7e1ba88a41a4de
ftCLI <ftCLI@proton.me>
2023-12-04 10:52:30 +0100

    Blacken code

--- foundrytools_cli_2/snippets/ps_recalc_zones.py
+++ foundrytools_cli_2/snippets/ps_recalc_zones.py
@@ -159,13 +159,12 @@ def calculate_zone(
 
 
 def recalc_zones(
-        font: TTFont,
-        descender_glyphs: t.List[str] = None,
-        baseline_glyphs: t.List[str] = None,
-        x_height_glyphs: t.List[str] = None,
-        uppercase_glyphs: t.List[str] = None,
-        ascender_glyphs: t.List[str] = None,
-
+    font: TTFont,
+    descender_glyphs: t.List[str] = None,
+    baseline_glyphs: t.List[str] = None,
+    x_height_glyphs: t.List[str] = None,
+    uppercase_glyphs: t.List[str] = None,
+    ascender_glyphs: t.List[str] = None,
 ) -> t.Tuple[t.List[int], t.List[int]]:
     """
     Recalc Zones
