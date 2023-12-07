ec07ccef41f869be1b6fedfda05511a8a66f9be5
ftCLI <ftCLI@proton.me>
2023-12-07 09:01:08 +0100

    Added docstrings

--- foundrytools_cli_2/lib/beziers/beziers_tools.py
+++ foundrytools_cli_2/lib/beziers/beziers_tools.py
@@ -1,4 +1,4 @@
-from typing import List
+import typing as t
 from beziers.path import BezierPath
 from fontTools.misc.psCharStrings import T2CharString
 from fontTools.ttLib import TTFont
@@ -6,6 +6,18 @@ from fontTools.pens.t2CharStringPen import T2CharStringPen
 
 
 def handle_curve(pen: T2CharStringPen, nodes_list: list, i: int) -> int:
+    """
+    Handles the curve nodes in a BezierPath.
+
+    Parameters:
+        pen (T2CharStringPen): The T2CharString pen to draw the curve.
+        nodes_list (list): The list of nodes in the BezierPath.
+        i (int): The starting index of the curve nodes.
+
+    Returns:
+        int: The index of the next node after the curve.
+
+    """
     curve_points = []
     while i < len(nodes_list) and nodes_list[i].type in {"offcurve", "curve"}:
         curve_points.append((nodes_list[i].x, nodes_list[i].y))
@@ -16,7 +28,21 @@ def handle_curve(pen: T2CharStringPen, nodes_list: list, i: int) -> int:
     return i + 1  # Ensure "curve" node is not processed again
 
 
-def bezier_to_charstring(paths: List[BezierPath], font: TTFont, glyph_name: str) -> T2CharString:
+def bezier_to_charstring(paths: t.List[BezierPath], font: TTFont, glyph_name: str) -> T2CharString:
+    """
+    Converts a list of Bezier paths to a T2CharString.
+
+    Parameters:
+        paths (list): The list of Bezier paths.
+        font (TTFont): The font.
+        glyph_name (str): The name of the glyph.
+
+    Returns:
+        T2CharString: The T2CharString.
+
+    Raises:
+        ValueError: If an unknown node type is encountered.
+    """
     glyph_set = font.getGlyphSet()
     pen = T2CharStringPen(width=glyph_set[glyph_name].width, glyphSet=glyph_set)
 
