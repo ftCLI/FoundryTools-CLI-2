e2c4cf9a37093331a436f8ec0608a7578a0a399e
ftCLI <ftCLI@proton.me>
2023-12-12 09:49:19 +0100

    Removed default value for `input_path` in `FontFinder`.

--- foundrytools_cli_2/lib/font_finder.py
+++ foundrytools_cli_2/lib/font_finder.py
@@ -13,7 +13,7 @@ class FinderOptions:
     A class that specifies the options to pass to the FontFinder class.
     """
 
-    input_path: Path = None
+    input_path: Path
     recursive: bool = False
     lazy: t.Optional[bool] = None
     recalc_bboxes: bool = True
