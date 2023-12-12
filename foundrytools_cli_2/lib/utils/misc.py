ff96cdb4871a78001fb1735fdae92168090517b9
ftCLI <ftCLI@proton.me>
2023-12-12 09:41:56 +0100

    `Add docstrings to functions in misc.py`

--- foundrytools_cli_2/lib/utils/misc.py
+++ foundrytools_cli_2/lib/utils/misc.py
@@ -12,12 +12,30 @@ def save_font(
     reorder_tables: t.Optional[bool] = True,
     suffix: str = "",
 ) -> None:
+    """
+    Save a font to a file.
+
+    Parameters:
+        font (Font): The font to save.
+        output_dir (Optional[Path], optional): The output directory. Defaults to None.
+        overwrite (bool, optional): Flag indicating whether to overwrite existing files.
+            Defaults to False.
+        reorder_tables (Optional[bool], optional): Flag indicating whether to reorder the font
+            tables. Defaults to True.
+        suffix (str, optional): The suffix to append to the output file name. Defaults to "".
+    """
     output_file = font.make_out_file_name(output_dir=output_dir, overwrite=overwrite, suffix=suffix)
     font.save(output_file, reorder_tables=reorder_tables)
     log_file_saved(output_file=output_file)
 
 
-def log_current_font(font: Font):
+def log_current_font(font: Font) -> None:
+    """
+    Logs the current font information.
+
+    Parameters:
+        font (Font): The font to log.
+    """
     if hasattr(font, "file"):
         logger.info(f"Current file: {font.file}")
     elif hasattr(font, "bytesio"):
@@ -26,9 +44,23 @@ def log_current_font(font: Font):
         logger.info(f"Current font: {font.ttfont}")
 
 
-def log_file_saved(output_file: Path):
+def log_file_saved(output_file: Path) -> None:
+    """
+    Log the information that a file has been saved to the specified output path.
+
+    Parameters:
+        output_file (Path): The path to the output file.
+    """
     logger.success(f"File saved to {output_file}")
 
 
-def log_file_not_saved(input_file: Path):
+def log_file_not_saved(input_file: Path) -> None:
+    """
+    Log File Not Saved
+
+    Logs a warning message indicating that no changes have been made to the specified input file.
+
+    Parameters:
+        input_file (Path): The path to the input file.
+    """
     logger.warning(f"No changes made to {input_file}")
