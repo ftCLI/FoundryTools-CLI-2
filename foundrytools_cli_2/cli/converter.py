bda69c457c835c8a1461da083ce3fa681dd6b19b
ftCLI <ftCLI@proton.me>
2023-12-27 22:23:12 +0100

    Renamed conversion functions for clarity in CLI tool
    Function names in converter.py have been made more explicit to improve readability and understanding of the code. The abbreviations used previously have been replaced with full versions such as "ps2tt" to "postscript_to_truetype", and "wf2ft" to "web_to_sfnt".

--- foundrytools_cli_2/cli/converter.py
+++ foundrytools_cli_2/cli/converter.py
@@ -25,7 +25,7 @@ cli = click.Group("converter", help="Font conversion utilities.")
 @tolerance_option()
 @target_upm_option(help_msg="Scale the converted fonts to the specified UPM.")
 @common_options()
-def ps2tt(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
+def postscript_to_truetype(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
     """
     Convert PostScript flavored fonts to TrueType flavored fonts.
     """
@@ -43,7 +43,7 @@ def ps2tt(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
 @target_upm_option(help_msg="Scale the converted fonts to the specified UPM.")
 @subroutinize_flag()
 @common_options()
-def tt2ps(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
+def truetype_to_postscript(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
     """
     Convert TrueType flavored fonts to PostScript flavored fonts.
     """
@@ -65,7 +65,7 @@ def tt2ps(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
 @cli.command("wf2ft")
 @in_format_choice()
 @common_options()
-def wf2ft(
+def web_to_sfnt(
     input_path: Path,
     in_format: t.Optional[t.Literal["woff", "woff2"]],
     **options: t.Dict[str, t.Any],
@@ -85,7 +85,7 @@ def wf2ft(
 @cli.command("ft2wf")
 @out_format_choice()
 @common_options()
-def ft2wf(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
+def sfnt_to_web(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
     """
     Convert SFNT fonts to WOFF and/or WOFF2 fonts.
     """
@@ -100,7 +100,7 @@ def ft2wf(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
 
 @cli.command("var2static")
 @common_options()
-def var2static(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
+def variable_to_static(input_path: Path, **options: t.Dict[str, t.Any]) -> None:
     """
     Convert variable fonts to static fonts.
     """
