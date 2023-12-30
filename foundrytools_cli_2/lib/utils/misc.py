78c898bac15451ac9691b5fcc5d283badeb12b81
ftCLI <ftCLI@proton.me>
2023-12-30 10:35:54 +0100

    Refactor utils/misc.py and simplify code
    Removed unused functions, such as `save_font` and `log_current_font`, and added a new function `get_temp_file_path` in utils/misc.py. This streamlines the code and provides a utility function to generate temporary file paths, thus improving code readability and efficiency.

--- foundrytools_cli_2/lib/utils/misc.py
+++ foundrytools_cli_2/lib/utils/misc.py
@@ -1,66 +1,13 @@
+import os
+import tempfile
 import typing as t
 from pathlib import Path
 
-from foundrytools_cli_2.lib.font import Font
-from foundrytools_cli_2.lib.logger import logger
 
-
-def save_font(
-    font: Font,
-    output_dir: t.Optional[Path] = None,
-    overwrite: bool = False,
-    reorder_tables: t.Optional[bool] = True,
-    suffix: str = "",
-) -> None:
+def get_temp_file_path(directory: t.Optional[t.Union[str, Path]] = None) -> Path:
     """
-    Save a font to a file.
-
-    Parameters:
-        font (Font): The font to save.
-        output_dir (Optional[Path], optional): The output directory. Defaults to None.
-        overwrite (bool, optional): Flag indicating whether to overwrite existing files.
-            Defaults to False.
-        reorder_tables (Optional[bool], optional): Flag indicating whether to reorder the font
-            tables. Defaults to True.
-        suffix (str, optional): The suffix to append to the output file name. Defaults to "".
+    Returns a temporary file path.
     """
-    output_file = font.make_out_file_name(output_dir=output_dir, overwrite=overwrite, suffix=suffix)
-    font.save(output_file, reorder_tables=reorder_tables)
-    log_file_saved(output_file=output_file)
-
-
-def log_current_font(font: Font) -> None:
-    """
-    Logs the current font information.
-
-    Parameters:
-        font (Font): The font to log.
-    """
-    if hasattr(font, "file"):
-        logger.info(f"Current file: {font.file}")
-    elif hasattr(font, "bytesio"):
-        logger.info(f"Current buffer: {font.bytesio}")
-    else:
-        logger.info(f"Current font: {font.ttfont}")
-
-
-def log_file_saved(output_file: Path) -> None:
-    """
-    Log the information that a file has been saved to the specified output path.
-
-    Parameters:
-        output_file (Path): The path to the output file.
-    """
-    logger.success(f"File saved to {output_file}")
-
-
-def log_file_not_saved(input_file: Path) -> None:
-    """
-    Log File Not Saved
-
-    Logs a warning message indicating that no changes have been made to the specified input file.
-
-    Parameters:
-        input_file (Path): The path to the input file.
-    """
-    logger.warning(f"No changes made to {input_file}")
+    file_descriptor, path = tempfile.mkstemp(dir=directory)
+    os.close(file_descriptor)
+    return Path(path)
