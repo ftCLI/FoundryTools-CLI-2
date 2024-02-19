28f13c07dbc18aa951d19f21e1fe5c09aaeda5f6
ftCLI <ftCLI@proton.me>
2024-02-19 11:58:48 +0100

    Refactor code organization

--- foundrytools_cli_2/lib/tables/head.py
+++ foundrytools_cli_2/lib/tables/head.py
@@ -1,7 +1,7 @@
 from fontTools.ttLib import TTFont
 
 from foundrytools_cli_2.lib.tables.default import DefaultTbl
-from foundrytools_cli_2.lib.utils.misc import is_nth_bit_set
+from foundrytools_cli_2.lib.utils.bits_tools import is_nth_bit_set
 
 
 class HeadTable(DefaultTbl):
--- foundrytools_cli_2/lib/utils/bits_tools.py
+++ foundrytools_cli_2/lib/utils/bits_tools.py
@@ -1,18 +1,3 @@
-import os
-import tempfile
-import typing as t
-from pathlib import Path
-
-
-def get_temp_file_path(directory: t.Optional[t.Union[str, Path]] = None) -> Path:
-    """
-    Returns a temporary file path.
-    """
-    file_descriptor, path = tempfile.mkstemp(dir=directory)
-    os.close(file_descriptor)
-    return Path(path)
-
-
 def is_nth_bit_set(x: int, n: int) -> bool:
     """
     If the nth bit of an integer x is set, return True, otherwise return False
