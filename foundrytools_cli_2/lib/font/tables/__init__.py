0532d13497ab6993189460bbdb76be752d78719b
ftCLI <ftCLI@proton.me>
2024-02-20 08:00:31 +0100

    Import InvalidOS2VersionError in os_2.py

--- foundrytools_cli_2/lib/font/tables/__init__.py
+++ foundrytools_cli_2/lib/font/tables/__init__.py
@@ -1,5 +1,5 @@
 from foundrytools_cli_2.lib.font.tables.head import HeadTable
 from foundrytools_cli_2.lib.font.tables.name import NameTable
-from foundrytools_cli_2.lib.font.tables.os_2 import OS2Table
+from foundrytools_cli_2.lib.font.tables.os_2 import InvalidOS2VersionError, OS2Table
 
-__all__ = ["HeadTable", "NameTable", "OS2Table"]
+__all__ = ["HeadTable", "NameTable", "OS2Table", "InvalidOS2VersionError"]
