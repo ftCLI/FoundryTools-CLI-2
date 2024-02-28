6b0e7d09471cec627d3d93a9a0a317cdb4ed499c
ftCLI <ftCLI@proton.me>
2024-02-28 11:30:01 +0100

    Add directory check in get_temp_file_path function

--- foundrytools_cli_2/lib/utils/path_tools.py
+++ foundrytools_cli_2/lib/utils/path_tools.py
@@ -3,11 +3,16 @@ import tempfile
 import typing as t
 from pathlib import Path
 
+from foundrytools_cli_2.lib import logger
+
 
 def get_temp_file_path(directory: t.Optional[t.Union[str, Path]] = None) -> Path:
     """
     Returns a temporary file path.
     """
+    if directory and not directory.is_dir():
+        logger.warning(f"{directory} is not a directory, using default temp directory")
+        directory = None
     file_descriptor, path = tempfile.mkstemp(dir=directory)
     os.close(file_descriptor)
     return Path(path)
