e55d21dcbddfb4784831e5ee04c85cea32fcc154
ftCLI <ftCLI@proton.me>
2024-03-01 12:31:15 +0100

    Add DefaultTbl to __init__.py in font tables

--- foundrytools_cli_2/lib/font/tables/__init__.py
+++ foundrytools_cli_2/lib/font/tables/__init__.py
@@ -1,6 +1,14 @@
+from foundrytools_cli_2.lib.font.tables.default import DefaultTbl
 from foundrytools_cli_2.lib.font.tables.gsub import GsubTable
 from foundrytools_cli_2.lib.font.tables.head import HeadTable
 from foundrytools_cli_2.lib.font.tables.name import NameTable
 from foundrytools_cli_2.lib.font.tables.os_2 import InvalidOS2VersionError, OS2Table
 
-__all__ = ["GsubTable", "HeadTable", "NameTable", "OS2Table", "InvalidOS2VersionError"]
+__all__ = [
+    "DefaultTbl",
+    "GsubTable",
+    "HeadTable",
+    "NameTable",
+    "OS2Table",
+    "InvalidOS2VersionError",
+]
