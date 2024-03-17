31c93821b1b68a458ada6747628f1044d0913177
ftCLI <ftCLI@proton.me>
2024-03-17 07:32:33 +0100

    Correct documentation reference in CFFTable class

--- foundrytools_cli_2/lib/font/tables/cff_.py
+++ foundrytools_cli_2/lib/font/tables/cff_.py
@@ -5,7 +5,7 @@ from foundrytools_cli_2.lib.font.tables.default import DefaultTbl
 
 class CFFTable(DefaultTbl):  # pylint: disable=too-few-public-methods
     """
-    This class extends the fontTools `CFF_` table to add some useful methods.
+    This class extends the fontTools `CFF ` table to add some useful methods.
     """
 
     def __init__(self, ttfont: TTFont) -> None:
