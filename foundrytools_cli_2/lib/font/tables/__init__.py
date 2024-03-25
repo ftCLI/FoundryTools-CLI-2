69362d93e45172466c5a403e18b6aee569d82204
ftCLI <ftCLI@proton.me>
2024-03-25 08:14:24 +0100

    Add HheaTable class to handle font's 'hhea' table

--- foundrytools_cli_2/lib/font/tables/__init__.py
+++ foundrytools_cli_2/lib/font/tables/__init__.py
@@ -1,6 +1,7 @@
 from foundrytools_cli_2.lib.font.tables.cff_ import CFFTable
 from foundrytools_cli_2.lib.font.tables.gsub import GsubTable
 from foundrytools_cli_2.lib.font.tables.head import HeadTable
+from foundrytools_cli_2.lib.font.tables.hhea import HheaTable
 from foundrytools_cli_2.lib.font.tables.name import NameTable
 from foundrytools_cli_2.lib.font.tables.os_2 import OS2Table
 from foundrytools_cli_2.lib.font.tables.post import PostTable
@@ -9,6 +10,7 @@ __all__ = [
     "CFFTable",
     "GsubTable",
     "HeadTable",
+    "HheaTable",
     "NameTable",
     "OS2Table",
     "PostTable",
