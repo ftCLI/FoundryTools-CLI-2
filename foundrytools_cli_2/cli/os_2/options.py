ad6878023ba9aeec09d9ed84b9cd06a907d26e3e
ftCLI <ftCLI@proton.me>
2024-03-27 09:42:59 +0100

    Add functionality to upgrade OS/2 table version
    The new functionality allows the upgrade of the OS/2 table to a specified version. If the target version is less or equal to the current one, the table remains unaltered.

--- foundrytools_cli_2/cli/os_2/options.py
+++ foundrytools_cli_2/cli/os_2/options.py
@@ -5,7 +5,7 @@ import click
 from foundrytools_cli_2.cli.shared_callbacks import choice_to_int_callback
 from foundrytools_cli_2.cli.shared_options import add_options
 
-__all__ = ["set_attrs_options", "set_fs_selection_options", "set_fs_type_options"]
+__all__ = ["set_attrs_options", "set_fs_selection_options", "set_fs_type_options", "target_version"]
 
 
 def weight_class() -> t.Callable:
@@ -609,6 +609,23 @@ def bitmap_embed_only() -> t.Callable:
         )
     ]
     return add_options(_bitmap_embed_only)
+
+
+def target_version() -> t.Callable:
+    """
+    Add the ``target_version`` option to a click command.
+    """
+    _target_version = [
+        click.option(
+            "-v",
+            "--target-version",
+            type=click.IntRange(1, 5),
+            help="""
+            The version of the OS/2 table to set.
+            """,
+        )
+    ]
+    return add_options(_target_version)
 
 
 def set_attrs_options() -> t.Callable:
