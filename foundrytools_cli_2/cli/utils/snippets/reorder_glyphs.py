a6760f54bcbd500bdcb6d4bb58e11dd9033cc9a1
ftCLI <ftCLI@proton.me>
2024-04-18 22:31:32 +0200

    Add glyph reordering functionality
    Introduced a new feature to the foundrytools_cli that allows for reordering of glyphs. This addition includes functions to create a new glyph order and to reorder the glyphs. The changes have been exposed as a "reorder-glyphs" command in the CLI interface.

---
+++ foundrytools_cli_2/cli/utils/snippets/reorder_glyphs.py
@@ -0,0 +1,32 @@
+import typing as t
+
+from foundrytools_cli_2.lib.font import Font
+
+
+def create_new_glyph_order(font: Font) -> t.List[str]:
+    """
+    Creates the new glyph order.
+    """
+    new_glyph_order: t.List[str] = []
+    encoded_glyphs = list(font.ttfont.getBestCmap().values())
+    unencoded_glyphs = [glyph_name for glyph_name in font.ttfont.getGlyphOrder() if
+                        glyph_name not in encoded_glyphs]
+
+    if ".notdef" in unencoded_glyphs:
+        new_glyph_order.extend([".notdef"])
+        unencoded_glyphs.remove(".notdef")
+
+    # Add support for other ordering methods
+    new_glyph_order.extend(sorted(encoded_glyphs))
+    new_glyph_order.extend(sorted(unencoded_glyphs))
+
+    return new_glyph_order
+
+
+def main(font: Font) -> None:
+    """
+    Reorders the glyphs
+    """
+    new_glyph_order = create_new_glyph_order(font)
+    font.ttfont.reorderGlyphs(new_glyph_order=new_glyph_order)
+    font.modified = True
