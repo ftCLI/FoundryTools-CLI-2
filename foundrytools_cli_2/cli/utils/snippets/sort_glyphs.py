dcbd9609abc5983d68c9bc5a93d9cf80abcef7f2
ftCLI <ftCLI@proton.me>
2024-05-13 10:04:59 +0200

    Refactor sortGlyphNames and remove redundant operations

--- foundrytools_cli_2/cli/utils/snippets/sort_glyphs.py
+++ foundrytools_cli_2/cli/utils/snippets/sort_glyphs.py
@@ -4,7 +4,6 @@ import defcon
 import extractor
 
 from foundrytools_cli_2.lib.font import Font
-from foundrytools_cli_2.lib.font.tables import CFFTable
 
 
 def create_new_glyph_order(font: Font) -> t.List[str]:
@@ -15,7 +14,10 @@ def create_new_glyph_order(font: Font) -> t.List[str]:
     extractor.extractUFO(
         font.file, destination=ufo, doFeatures=False, doInfo=False, doKerning=False
     )
-    new_glyph_order = ufo.unicodeData.sortGlyphNames(glyphNames=font.ttfont.getGlyphOrder())
+    new_glyph_order = ufo.unicodeData.sortGlyphNames(
+        glyphNames=font.ttfont.getGlyphOrder(),
+        sortDescriptors=[dict(type="unicode")],
+    )
     if ".notdef" in new_glyph_order:
         new_glyph_order.remove(".notdef")
         new_glyph_order.insert(0, ".notdef")
@@ -28,19 +30,9 @@ def main(font: Font) -> None:
     Reorders the glyphs based on the Unicode values.
     """
 
-    ufo = defcon.Font()
-    extractor.extractUFO(
-        font.file, destination=ufo, doFeatures=False, doInfo=False, doKerning=False
-    )
     original_glyph_order = font.ttfont.getGlyphOrder()
     new_glyph_order = create_new_glyph_order(font)
-    font.ttfont.reorderGlyphs(new_glyph_order=new_glyph_order)
-    font.ttfont.setGlyphOrder(new_glyph_order)
-
-    if font.is_ps:
-        cff_table = CFFTable(font.ttfont)
-        charstrings = cff_table.charstrings.charStrings
-        cff_table.top_dict.charset = new_glyph_order
-        cff_table.charstrings.charStrings = {k: charstrings.get(k) for k in new_glyph_order}
-
-    font.modified = new_glyph_order != original_glyph_order
+    if new_glyph_order == original_glyph_order:
+        return
+    font.sort_glyphs(new_glyph_order=new_glyph_order)
+    font.modified = True
