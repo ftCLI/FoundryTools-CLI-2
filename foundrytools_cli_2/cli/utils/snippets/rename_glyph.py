36e96bb60cd642dc6135b14f86d5ff8d65cd1f1a
ftCLI <ftCLI@proton.me>
2024-07-08 11:58:57 +0200

    Update docstring in rename_glyph function

--- foundrytools_cli_2/cli/utils/snippets/rename_glyph.py
+++ foundrytools_cli_2/cli/utils/snippets/rename_glyph.py
@@ -3,7 +3,7 @@ from foundrytools_cli_2.lib.font import Font
 
 def main(font: Font, old_name: str, new_name:str) -> None:
     """
-    Set the production names of glyphs in a font.
+    Rename a single glyph in a font file.
 
     Args:
         font (Font): The font to rename the glyph in.
@@ -12,5 +12,4 @@ def main(font: Font, old_name: str, new_name:str) -> None:
     """
     result = font.rename_glyph(old_name=old_name, new_name=new_name)
     if result:
-        font.rebuild_cmap(remap_all=True)
         font.modified = True
