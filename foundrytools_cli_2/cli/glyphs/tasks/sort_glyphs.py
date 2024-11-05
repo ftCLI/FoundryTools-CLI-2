5dbaa9a62359e174f3eacf972f69033cbf2c2306
ftCLI <ftCLI@proton.me>
2024-11-05 11:18:47 +0100

    Refactor glyph sorting logic
    Removed unnecessary dependencies on defcon and extractor. Simplified the glyph sorting logic to rely on existing font properties.

--- foundrytools_cli_2/cli/glyphs/tasks/sort_glyphs.py
+++ foundrytools_cli_2/cli/glyphs/tasks/sort_glyphs.py
@@ -1,31 +1,6 @@
 import typing as t
 
-import defcon
-import extractor
-
 from foundrytools_cli_2.lib.font import Font
-
-
-def create_new_glyph_order(
-    font: Font,
-    sort_method: t.Literal["unicode", "alphabetical", "cannedDesign"] = "unicode",
-) -> t.List[str]:
-    """
-    Creates the new glyph order.
-    """
-    ufo = defcon.Font()
-    extractor.extractUFO(
-        font.file, destination=ufo, doFeatures=False, doInfo=False, doKerning=False
-    )
-    new_glyph_order = ufo.unicodeData.sortGlyphNames(
-        glyphNames=font.ttfont.getGlyphOrder(),
-        sortDescriptors=[{"type": sort_method}],
-    )
-    if ".notdef" in new_glyph_order:
-        new_glyph_order.remove(".notdef")
-        new_glyph_order.insert(0, ".notdef")
-
-    return new_glyph_order
 
 
 def main(
