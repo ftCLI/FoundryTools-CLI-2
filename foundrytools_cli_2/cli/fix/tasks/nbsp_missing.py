a96f5068adb5642c688860671a91469525cc8974
ftCLI <ftCLI@proton.me>
2025-01-06 23:16:16 +0100

    Refactor commands

--- foundrytools_cli_2/cli/fix/tasks/nbsp_missing.py
+++
@@ -1,9 +0,0 @@
-from foundrytools import Font
-
-
-def main(font: Font) -> bool:
-    """
-    Fixes the missing non-breaking space glyph by double mapping the space glyph.
-    """
-    font.t_cmap.add_missing_nbsp()
-    return font.t_cmap.is_modified
